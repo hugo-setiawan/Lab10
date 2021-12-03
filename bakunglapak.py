4c6c39eec3a592423494efa176bf69170a191885
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-12-03 11:34:37 +0700

    add reference

--- bakunglapak.py
+++ bakunglapak.py
@@ -279,3 +279,4 @@ if __name__ == "__main__":
 # https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/
 # https://stackoverflow.com/a/1101765
 # https://www.pythontutorial.net/tkinter/tkinter-askretrycancel/
+# https://stackoverflow.com/a/21006752
